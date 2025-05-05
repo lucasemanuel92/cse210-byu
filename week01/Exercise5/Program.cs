d3743c89d12167f540eef15cbb8677e218755f62
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2025-05-05 17:49:11 -0300

    Update Program.cs

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,43 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome() {
+        Console.WriteLine("Welcome to the Program");
+    }           
+    
+    // I will recive a string, this is why I use static string
+    static string PromptUserName() {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    // Same idea with the int
+    static int PromptUserNumber() {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    // Same idea the one before
+    static int SquareNumber(int number) {
+        int squareNumber = number * number;
+        return squareNumber;
+    }
+
+    static void DisplayResults(string userName, int squareNumber) {
+        Console.WriteLine($"{userName}, the square number of your number is {squareNumber}");
+    }
+
+    // Attention on how to declare the functions here
+    // type returner = functions name
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int number = PromptUserNumber();
+        int squareNumber = SquareNumber(number);
+        DisplayResults(userName, squareNumber);
     }
+
 }
