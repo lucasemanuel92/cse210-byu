f7b23f5c1d68b545db64a3af7dd09b6b9ca1f398
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2025-05-05 17:48:07 -0300

    Update Program.cs

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+          Console.Write("Insert the grade: ");
+        // Attach the input to a string
+        string input = Console.ReadLine();
+        // Convert the string to an int
+        int grade = int.Parse(input);
+
+        string finalGrade = "";
+        string signal = "";
+
+        // Starts the conditionals for the grade
+        if (grade >= 90) {
+            finalGrade = "A";
+        }
+        else if (grade >= 80) {
+            finalGrade = "B";
+        }
+        else if (grade >= 70) {
+            finalGrade = "C";
+        }
+        else if (grade >= 60) {
+            finalGrade = "D";
+        }
+        else if (grade < 60) {
+            finalGrade = "F";
+        }
+
+        // Creating the system to set the + or - and treating the execptions.
+        int lastDigit = grade % 10;
+        // Last digit, not A or F and not F for the second
+        if (lastDigit >= 7 && finalGrade != "A" && finalGrade!= "F") {
+            signal = "+";
+        } else if (lastDigit <= 3 && finalGrade!= "F") {
+            signal = "-";
+        } else {
+            signal = "";
+        }
+
+        if (grade >= 70) {
+            Console.WriteLine($"Congratulations, you passed. Your grade was {finalGrade}{signal}");
+        } else {
+            Console.WriteLine($"You have not been approved, your grade was {finalGrade}");
+        }
     }
 }
