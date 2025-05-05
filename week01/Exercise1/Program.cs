5b2de2f2a56003ba590c2376b7779d5b05105caf
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2025-05-05 17:47:45 -0300

    Update Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Creates a box to insert the data 
+       Console.Write("What is your first name? ");
+       // Use the data inserted and create a type attached to it
+       string firstName = Console.ReadLine();
+
+       Console.Write("What is your last name? ");
+       string lastName = Console.ReadLine();
+
+       // Remember to use the "$" to use the {} in the console. 
+       Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
