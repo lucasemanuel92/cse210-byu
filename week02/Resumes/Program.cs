7bbe51febcf2f4a4634642e9423cc160525b657b
Lucas <lucas.emanuel.carvalho@outlook.com>
2025-05-19 16:12:46 -0300

    Resume - 2nd part

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Maths and Physics Private Tutor";
+        job1._company = "Êxito Acompanhamento Escolar";
+        job1._startYear = 2014;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Full Stack Intern";
+        job2._company = "Keep It Simple Solutions";
+        job2._startYear = 2024;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Lucas Carvalho";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
